b9f14e167b424a1500449ba823f912bcfb139e5a
munaalsahli <monamuhammed70@gmail.com>
2017-10-06 17:12:08 +0300

    updated

---
+++ frontend-nanodegree-arcade-game/frontend-nanodegree-arcade-game 4/README.md
@@ -0,0 +1,21 @@
+
+
+
+frontend-nanodegree-arcade-game
+
+###Original Instructions
+
+Students should use this rubric for self-checking their submission. Make sure the functions you write are object-oriented - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the readme.md file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this guide.
+
+###General Usage
+
+Download files, or clone repository [https://github.com/munaalsahli/arcadeGame.git]
+Open index.html
+Use arrow keys to control avatar.
+###Game Rules
+
+Navigate through the barrage of bugs to arrive safely at the water.
+If you come into contact with the bugs you start over.
+Once you reach the other side you also start over.
---
+++ frontend-nanodegree-arcade-game/frontend-nanodegree-arcade-game 4/js/app.js
@@ -0,0 +1,136 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+  this.x += (this.speed) * dt;
+
+ // When bugs go off screen, move them back to start position
+ if (this.x > 400) {
+     this.x = -100;
+ }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+  this.sprite = 'images/char-pink-girl.png';
+};
+
+
+Player.prototype.update = function() {
+    // setting up the boundries
+displayScore(score,gameLevel);
+    for (i=0; i < allEnemies.length; i++) {
+        if (this.x < allEnemies[i].x + 60 && this.x + 60 > allEnemies[i].x && this.y < allEnemies[i].y + 60 && 60 + this.y > allEnemies[i].y) {
+            this.y = 320;
+            this.x = 200;
+        }
+    }
+
+    // If the player reaches the water, restart the player by creating a new player.
+    if (this.y < 0) {
+        this.y = 320;
+        this.x = 200;
+        score += 1;
+        gameLevel += 1;
+        console.log('current score: ' + score + ', current level: ' + gameLevel);
+
+        increaseDifficulty(score);
+    }
+};
+
+// Draw the player on the screen, required method for game
+// Display score
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    //displayScoreLevel(score, gameLevel);
+
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress == 'left') {
+        this.x -= this.speed;
+    }
+    if (keyPress == 'up') {
+        this.y -= this.speed - 20;
+    }
+    if (keyPress == 'right') {
+        this.x += this.speed;
+    }
+    if (keyPress == 'down') {
+        this.y += this.speed - 20;
+    }
+    console.log('keyPress is: ' + keyPress);
+};
+
+// Function to display player's score
+displayScore = function(aScore, aLevel) {
+    var canvas = document.getElementsByTagName('canvas');
+    var firstCanvasTag = canvas[0];
+
+    // add player score and level to div element created
+    scoreLevelDiv.innerHTML = 'Score: ' + aScore
+        + ' / ' + 'Level: ' + aLevel;
+    document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
+};
+// Increase number of enemies on screen based on player's score
+var increaseDifficulty = function(numEnemies) {
+    // remove all previous enemies on canvas
+    allEnemies.length = 0;
+
+    // load new set of enemies
+    for (var i = 0; i <= numEnemies; i++) {
+        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+
+        allEnemies.push(enemy);
+    }
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+// Enemy randomly placed vertically within section of canvas
+// Declare new score and gameLevel variables to store score and level
+var allEnemies = [];
+var player = new Player(202.5, 383, 50);
+var score = 0;
+var gameLevel = 1;
+var scoreLevelDiv = document.createElement('div');
+var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+
+allEnemies.push(enemy);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keydown', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+    console.log(allowedKeys[e.keyCode]);
+});
